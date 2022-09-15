3f5525abbef13b8e93124889a0bbe0e156e02b7e
Robby Kurniawan <thelazpiz@gmail.com>
2022-09-15 21:26:19 +0700

    belum selesai

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
+let form = document.getElementById("form");
+let username = document.getElementById("username");
+let post = document.getElementById("post");
+let thumbpost = document.getElementById("thumbpost");
+
+form.addEventListener("button", (e) => {
+    e.preventDefault();
+    console.log("button clicked");
+  
+    eventPost();
+});
+
 function eventPost(){
-//kode post
+//kode username
+    if (username.value === "") {
+        post.innerHTML = "Please insert a username";
+        console.log("failure");
+    } else {
+        console.log("successs");
+        post.innerHTML = "";
+        acceptData();
+    }
 }
+
+let data = {};
+
+function acceptData() {
+    data["text"] = username.value;
+    console.log(data);
+    createPost();
+};
+
+function createPost() {
+    thumbpost.innerHTML = data.text;
+};
 
 function deletePost(){
     
